cb4af28eb5a54bb34d98b16cc49bd8effb444d68
Simon de Boer <34236412+Simonwdb@users.noreply.github.com>
2026-04-21 13:06:45 +0200

    add _determine_days method to nearest neighbour solver v2

--- src/Validator/NearestNeighbourSolverV2.py
+++ src/Validator/NearestNeighbourSolverV2.py
@@ -12,4 +12,42 @@ class NearestNeighbourSolverV2(BaseSolver):
     - This allows combining deliveries and pickups in one route
     """
 
-    pass
+    def _determine_days(self):
+        """
+        Determines delivery and pickup days for each request
+        using the FeasibleGreedy approach.
+        Returns two dicts: delivery_day and pickup_day per request ID.
+        """
+        num_tools = len(self.instance.Tools)
+        tool_use = [[0] * (self.instance.Days + 2) for _ in range(num_tools + 1)]
+        delivery_day = {}
+        pickup_day = {}
+
+        sorted_requests = sorted(self.instance.Requests, key=lambda r: r.fromDay)
+
+        for request in sorted_requests:
+            day = request.fromDay
+
+            while day <= request.toDay:
+                pickup = day + request.numDays
+                violation = False
+                for d in range(day, pickup + 1):
+                    if tool_use[request.tool][d] + request.toolCount > self.instance.Tools[request.tool - 1].amount:
+                        violation = True
+                        break
+                if not violation:
+                    break
+                day += 1
+
+            if day > request.toDay:
+                print(f"WARNING: Request {request.ID} could not be planned!")
+                continue
+
+            pickup = day + request.numDays
+            for d in range(day, pickup + 1):
+                tool_use[request.tool][d] += request.toolCount
+
+            delivery_day[request.ID] = day
+            pickup_day[request.ID] = pickup
+
+        return delivery_day, pickup_day
